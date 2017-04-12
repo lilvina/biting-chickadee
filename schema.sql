4171459ed5820fcbd33b67eed7c2dc6a03b9a012
lilvina <davinaleo2007@yahoo.com>
2017-04-12 12:25:06 -0700

    adds the column definitions for the rest of our tables

--- schema.sql
+++ schema.sql
@@ -50,37 +50,83 @@ CREATE TABLE order (
   completed VARCHAR 255,
   payment_method VARCHAR 255,
   date_time TIMESTAMP,
-  total 
+  total DECIMAL
 );
 
 
 DROP TABLE IF EXISTS delivery;
-CREATE TABLE delivery;
+CREATE TABLE delivery (
+  id SERIAL PRIMARY KEY,
+  status VARCHAR 255,
+  delivery_address FOREIGN KEY,
+  delivery_person VARCHAR 255,
+  order_id FOREIGN KEY,
+  price INTEGER,
+  tip INTEGER
+);
 
 
 DROP TABLE IF EXISTS address;
-CREATE TABLE address;
+CREATE TABLE address (
+  id SERIAL PRIMARY KEY,
+  street_address VARCHAR 255,
+  zipcode INTEGER,
+  apartment_number VARCHAR 255,
+  city VARCHAR 255,
+  state VARCHAR 255
+);
 
 
 DROP TABLE IF EXISTS drink;
-CREATE TABLE drink;
+CREATE TABLE drink (
+  id SERIAL PRIMARY KEY,
+  description VARCHAR 255,
+  manufacturer VARCHAR 255,
+  supplier VARCHAR 255,
+  price DECIMAL
+);
 
 
 DROP TABLE IF EXISTS drink_order;
-CREATE TABLE drink_order;
+CREATE TABLE drink_order (
+  id SERIAL PRIMARY KEY,
+  order_id FOREIGN KEY,
+  drink_id FOREIGN KEY,
+  quantity INTEGER
+);
 
 
 DROP TABLE IF EXISTS pizza;
-CREATE TABLE pizza;
+CREATE TABLE pizza (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR 255,
+  size VARCHAR 255,
+  type VARCHAR 255,
+  price DECIMAL,
+  discount DECIMAL
+);
 
 
 DROP TABLE IF EXISTS pizza_order;
-CREATE TABLE pizza_order;
+CREATE TABLE pizza_order (
+  id SERIAL PRIMARY KEY,
+  order_id FOREIGN KEY,
+  pizza_id FOREIGN KEY,
+  quantity INTEGER
+);
 
 
 DROP TABLE IF EXISTS pizza_ingredient;
-CREATE TABLE pizza_ingredient;
+CREATE TABLE pizza_ingredient (
+  pizza_id FOREIGN KEY,
+  ingredient_id FOREIGN KEY
+);
 
 
 DROP TABLE IF EXISTS ingredient;
-CREATE TABLE ingredient;
+CREATE TABLE ingredient (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR 255,
+  price DECIMAL,
+  quantity INTEGER
+);
