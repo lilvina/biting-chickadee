cad34058713663be08c3ed215ef62587791c1101
Monica Williams <monicaestellaw@gmail.com>
2017-04-12 11:38:01 -0700

    drops and creates tables for database

---
+++ schema.sql
@@ -0,0 +1,55 @@
+DROP DATABASE IF EXISTS PiccadillyPizza;
+
+CREATE DATABASE PiccadillyPizza;
+
+
+DROP TABLE IF EXISTS customer;
+CREATE TABLE customer;
+
+
+DROP TABLE IF EXISTS credit_card;
+CREATE TABLE credit_card;
+
+
+DROP TABLE IF EXISTS customer_address;
+CREATE TABLE customer_address;
+
+
+DROP TABLE IF EXISTS billing_address;
+CREATE TABLE billing_address;
+
+
+DROP TABLE IF EXISTS order;
+CREATE TABLE order;
+
+
+DROP TABLE IF EXISTS delivery;
+CREATE TABLE delivery;
+
+
+DROP TABLE IF EXISTS address;
+CREATE TABLE address;
+
+
+DROP TABLE IF EXISTS drink;
+CREATE TABLE drink;
+
+
+DROP TABLE IF EXISTS drink_order;
+CREATE TABLE drink_order;
+
+
+DROP TABLE IF EXISTS pizza;
+CREATE TABLE pizza;
+
+
+DROP TABLE IF EXISTS pizza_order;
+CREATE TABLE pizza_order;
+
+
+DROP TABLE IF EXISTS pizza_ingredient;
+CREATE TABLE pizza_ingredient;
+
+
+DROP TABLE IF EXISTS ingredient;
+CREATE TABLE ingredient;
