e50c7f09c7b3855315785d146e825c50db833498
lilvina <davinaleo2007@yahoo.com>
2017-04-13 11:52:14 -0700

    added null and not null constraints to all appropriate columns for the sake of being explicit

--- schema.sql
+++ schema.sql
@@ -6,119 +6,118 @@ CREATE DATABASE PiccadillyPizza;
 DROP TABLE IF EXISTS customer;
 CREATE TABLE customer (
   id SERIAL PRIMARY KEY,
-  name VARCHAR ( 255 ),
-  username VARCHAR ( 255 ) NULL,
-  password VARCHAR ( 255 ) NULL,
+  name VARCHAR ( 255 ) NOT NULL,
+  username VARCHAR ( 255 ) NOT NULL,
+  password VARCHAR ( 255 ) NOT NULL,
   phone_number INTEGER NULL
 );
 
 DROP TABLE IF EXISTS credit_card;
 CREATE TABLE credit_card (
   id SERIAL PRIMARY KEY,
-  customer_name VARCHAR ( 255 ) NULL,
-  card_number VARCHAR ( 16 ) NULL,
-  billing_address VARCHAR ( 255 ) NULL,
-  apartment_number VARCHAR ( 255 ),
-  zipcode INTEGER,
-  customer_id INTEGER REFERENCES customer ( id ),
-  expiration_date TIMESTAMP NULL,
-  card_type VARCHAR ( 255 ),
-  city VARCHAR ( 255 ),
-  state VARCHAR ( 255 )
+  customer_name VARCHAR ( 255 ) NOT NULL,
+  card_number VARCHAR ( 16 ) NOT NULL,
+  billing_address VARCHAR ( 255 ) NOT NULL,
+  apartment_number VARCHAR ( 255 ) NULL,
+  zipcode INTEGER NOT NULL,
+  customer_id INTEGER REFERENCES customer ( id ) NOT NULL,
+  expiration_date TIMESTAMP NOT NULL,
+  card_type VARCHAR ( 255 ) NOT NULL,
+  city VARCHAR ( 255 ) NOT NULL,
+  state VARCHAR ( 255 ) NOT NULL
 );
 
 DROP TABLE IF EXISTS customer_address;
 CREATE TABLE customer_address (
   id SERIAL PRIMARY KEY,
-  customer_id INTEGER REFERENCES customer ( id ),
-  address_id INTEGER REFERENCES billing_address ( id )
+  customer_id INTEGER REFERENCES customer ( id ) NOT NULL,
+  address_id INTEGER REFERENCES billing_address ( id ) NOT NULL
 );
 
 DROP TABLE IF EXISTS purchase;
 CREATE TABLE purchase (
   id SERIAL PRIMARY KEY,
-  customer_id INTEGER REFERENCES customer ( id ),
-  completed BOOLEAN,
-  payment_method VARCHAR ( 255 ),
-  date_time TIMESTAMP,
-  total DECIMAL
+  customer_id INTEGER REFERENCES customer ( id ) NOT NULL,
+  completed BOOLEAN NOT NULL,
+  payment_method VARCHAR ( 255 ) NOT NULL,
+  date_time TIMESTAMP NOT NULL,
+  total DECIMAL NOT NULL
 );
 
 
 DROP TABLE IF EXISTS delivery;
 CREATE TABLE delivery (
   id SERIAL PRIMARY KEY,
-  status VARCHAR ( 255 ),
-  delivery_address INTEGER REFERENCES customer_address ( id ),
-  delivery_person VARCHAR ( 255 ),
-  -- consider delivery_person
-  purchase_id INTEGER REFERENCES purchase ( id ),
-  price DECIMAL,
-  tip DECIMAL
+  status VARCHAR ( 255 ) NOT NULL,
+  delivery_address INTEGER REFERENCES customer_address ( id ) NOT NULL,
+  delivery_person VARCHAR ( 255 ) NOT NULL,
+  purchase_id INTEGER REFERENCES purchase ( id ) NOT NULL,
+  price DECIMAL NOT NULL,
+  tip DECIMAL NULL
 );
 
 
 DROP TABLE IF EXISTS address;
 CREATE TABLE address (
   id SERIAL PRIMARY KEY,
-  street_address VARCHAR ( 255 ),
-  zipcode INTEGER,
-  apartment_number VARCHAR ( 255 ),
-  city VARCHAR ( 255 ),
-  state VARCHAR ( 255 )
+  street_address VARCHAR ( 255 ) NOT NULL,
+  zipcode INTEGER NOT NULL,
+  apartment_number VARCHAR ( 255 ) NOT NULL,
+  city VARCHAR ( 255 ) NOT NULL,
+  state VARCHAR ( 255 ) NOT NULL
 );
 
 
 DROP TABLE IF EXISTS drink;
 CREATE TABLE drink (
   id SERIAL PRIMARY KEY,
-  description VARCHAR ( 255 ),
-  manufacturer VARCHAR ( 255 ),
-  supplier VARCHAR ( 255 ),
-  price DECIMAL
+  description VARCHAR ( 255 ) NOT NULL,
+  manufacturer VARCHAR ( 255 ) NOT NULL,
+  supplier VARCHAR ( 255 ) NOT NULL,
+  price DECIMAL NOT NULL
 );
 
 
 DROP TABLE IF EXISTS drink_purchase;
 CREATE TABLE drink_purchase (
   id SERIAL PRIMARY KEY,
-  purchase_id INTEGER REFERENCES purchase ( id ),
-  drink_id INTEGER REFERENCES drink ( id ),
-  quantity INTEGER
+  purchase_id INTEGER REFERENCES purchase ( id ) NOT NULL,
+  drink_id INTEGER REFERENCES drink ( id ) NOT NULL,
+  quantity INTEGER NOT NULL
 );
 
 
 DROP TABLE IF EXISTS pizza;
 CREATE TABLE pizza (
   id SERIAL PRIMARY KEY,
-  name VARCHAR ( 255 ),
-  size VARCHAR ( 255 ),
-  type VARCHAR ( 255 ),
-  price DECIMAL,
-  discount DECIMAL
+  name VARCHAR ( 255 ) NOT NULL,
+  size VARCHAR ( 255 ) NOT NULL,
+  type VARCHAR ( 255 ) NOT NULL,
+  price DECIMAL NOT NULL,
+  discount DECIMAL NULL
 );
 
 
 DROP TABLE IF EXISTS pizza_purchase;
 CREATE TABLE pizza_purchase (
   id SERIAL PRIMARY KEY,
-  purchase_id INTEGER REFERENCES purchase ( id ),
-  pizza_id INTEGER REFERENCES pizza ( id ),
-  quantity INTEGER
+  purchase_id INTEGER REFERENCES purchase ( id ) NOT NULL,
+  pizza_id INTEGER REFERENCES pizza ( id ) NOT NULL,
+  quantity INTEGER NOT NULL
 );
 
 
 DROP TABLE IF EXISTS pizza_ingredient;
 CREATE TABLE pizza_ingredient (
-  pizza_id INTEGER REFERENCES pizza ( id ),
-  ingredient_id INTEGER REFERENCES customer ( id )
+  pizza_id INTEGER REFERENCES pizza ( id ) NOT NULL,
+  ingredient_id INTEGER REFERENCES customer ( id ) NOT NULL
 );
 
 
 DROP TABLE IF EXISTS ingredient;
 CREATE TABLE ingredient (
   id SERIAL PRIMARY KEY,
-  name VARCHAR ( 255 ),
-  price DECIMAL,
-  in_stock INTEGER
+  name VARCHAR ( 255 ) NOT NULL,
+  price DECIMAL NOT NULL,
+  in_stock INTEGER NOT NULL
 );
