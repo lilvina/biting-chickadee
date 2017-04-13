e8a41023957fa80977077a3aa883a6a107b7ccce
lilvina <davinaleo2007@yahoo.com>
2017-04-13 12:01:17 -0700

    adds insert script to add seed data into database; insertscript works

--- seed.sql
+++ seed.sql
@@ -5,24 +5,24 @@ VALUES ( 1, 'Beyoncé Knowles', 'bknowles', 'abacus', 1234567 ),
 ( 4, 'Michelle Williams', 'mwilliams', 'abashed', 4234567 ),
 ( 5, 'Blue Ivy Carter', 'bcarter', 'abate', 5234567 ),
 ( 6, 'Rupi Kaur', 'rkaur', 'abbrivated', 6234567 ),
-( 7, 'Michael Jackson', 'mjackson', 'moonwalker', 7234567 )
+( 7, 'Michael Jackson', 'mjackson', 'moonwalker', 7234567 ),
 ( 8, 'Barack Obama', 'bobama', 'renegade', 8234567 ),
 ( 9, 'Michelle Obama', 'mobama', 'renaissance', 9234567 ),
 ( 10, 'Malia Obama', 'mobama', 'rosebud', 1134567 ),
 ( 11, 'Sasha Obama', 'sobama', 'radiance', 1334567 );
 
-INSERT INTO credit_card ( 
-  id, 
-  customer_name, 
-  card_number, 
-  apartment_number,
-  street_address,
-  zipcode,
-  customer_id,
-  expiration_date,
-  card_type,
-  city,
-  state )
-VALUES (
-  1, 'Beyoncé Knowles', 4567390482903889,  
-)
+-- INSERT INTO credit_card ( 
+--   id, 
+--   customer_name, 
+--   card_number, 
+--   apartment_number,
+--   street_address,
+--   zipcode,
+--   customer_id,
+--   expiration_date,
+--   card_type,
+--   city,
+--   state )
+-- VALUES (
+--   1, 'Beyoncé Knowles', 4567390482903889, 
+-- )
